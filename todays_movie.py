2958e7d430327c0a37944d40211e2c23f9252631
trevorbonas <trevorbonas@gmail.com>
2022-01-12 17:09:38 -0800

    Add new movies and show watched option

--- todays_movie.py
+++ todays_movie.py
@@ -3,54 +3,52 @@ import random
 import readline
 import numpy as numpy
 
-input_file = open("/Users/trevorbonas/Desktop/Todays_Movie/movies.txt")
-
-len = 0
+input_file = open("movies.txt")
 
 movies = []
 
 for line in input_file:
 	datalist = line.splitlines()
-
 	movies += datalist
 
-
-#print(movies)
-
-ask = input("Generate movie? [yn]\n")
-
-if not ("y" in ask or "Y" in ask or "Yes" in ask or "YES" in ask):
-	print("Okay, goodbye")
-	exit()
-
 reserved = []
-reserved = numpy.loadtxt("/Users/trevorbonas/Desktop/Todays_Movie/movie_history.npy", dtype=int)
+reserved = numpy.loadtxt("movie_history.npy", dtype=int)
 
-#with open("movie_history.txt") as f:
-#		reserved += f.readlines()
+def printWatched(reserved, movies):
+	for num in reserved:
+		print(str(num) + " " + movies[num])
+	print(str(len(reserved))  + "/" + str(len(movies)) + "\n")
 
-rand_index = 0
-while True:
-	rand_index = random.randint(0, 1209)
-	if rand_index not in reserved:
-		break
-	if rand_index in reserved and reserved.size == 1222:
-		print("You have watched all 1222 movies")
-		exit()
+def genMovie(reserved, movies):
+	rand_index = 0
+	while True:
+		rand_index = random.randint(0, 1209)
+		if rand_index not in reserved:
+			break
+		if rand_index in reserved and reserved.size == movies.size:
+			print("You have watched all " + str(movies.size) + " movies")
+			exit()
 
-reserved = numpy.append(reserved, rand_index)
+	reserved = numpy.append(reserved, rand_index)
 
-numpy.savetxt("/Users/trevorbonas/Desktop/Todays_Movie/movie_history.npy", reserved, fmt='%d')
+	numpy.savetxt("movie_history.npy", reserved, fmt='%d')
 
-#print(reserved.size)
+	todays_movie = movies[rand_index]
 
-#print(reserved)
+	print("Today's movie is:\n" + todays_movie)
+	print("Number " + str(len(reserved)) + " of 1222\n")
+
+ask = ""
+while ask != "Q" and ask != "q":
+	ask = input("[G] generate movie\n[W] show watched movies\n[Q] quit\n> ")
+	if ask == "G" or ask == "g":
+		genMovie(reserved, movies)
+	elif ask == "W" or ask == "w":
+		printWatched(reserved, movies)
+
+print("Goodbye")
 
 
-todays_movie = movies[rand_index]
-
-print("Today's movie is:\n" + todays_movie)
-print("Number " + str(reserved.size) + " of 1222")
 
 
 
